3829f34401fc67d66214ec7ad0e4cf0b7309ff41
DanilAstroid | vdolya <113015972+vazhka-dolya@users.noreply.github.com>
2025-01-22 22:17:52 +0200

    Panel for angle; 360°; Russian translation
    - In place of the slider is now a panel that enables for better control over Mario's angle; the angle can now flip from 360 to 0 and vice versa, allowing you to keep manually spinning Mario non-stop.
    - The Auto Control feature now supports spinning up to 360 (technically 359) degrees per frame; its functionality for flipping when more than 360 or less than 0 has been improved to accommodate for that.
    - Added a Russian translation.
    - changed the About menu because damn that thing was almost a fossil

--- METALTwister/AngleBlaster.Designer.cs
+++ METALTwister/AngleBlaster.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AngleBlaster));
-            this.tbAngle = new System.Windows.Forms.TrackBar();
             this.btnSet = new System.Windows.Forms.Button();
             this.nudBlaster = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.gbMain = new System.Windows.Forms.GroupBox();
+            this.pnlRotBox = new System.Windows.Forms.Panel();
             this.btnFromGame = new System.Windows.Forms.Button();
             this.nudSpecificSet = new System.Windows.Forms.NumericUpDown();
             this.pbAngleDisplay = new System.Windows.Forms.PictureBox();
@@ -45,7 +45,6 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.tbAngle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudBlaster)).BeginInit();
             this.gbMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudSpecificSet)).BeginInit();
@@ -55,193 +54,146 @@
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tbAngle
-            // 
-            this.tbAngle.Location = new System.Drawing.Point(112, 74);
-            this.tbAngle.Maximum = 3600;
-            this.tbAngle.Name = "tbAngle";
-            this.tbAngle.Size = new System.Drawing.Size(361, 45);
-            this.tbAngle.TabIndex = 11;
-            this.tbAngle.TickStyle = System.Windows.Forms.TickStyle.None;
-            this.tbAngle.ValueChanged += new System.EventHandler(this.tbAngle_ValueChanged);
-            // 
             // btnSet
             // 
-            this.btnSet.Location = new System.Drawing.Point(251, 48);
+            resources.ApplyResources(this.btnSet, "btnSet");
             this.btnSet.Name = "btnSet";
-            this.btnSet.Size = new System.Drawing.Size(79, 20);
-            this.btnSet.TabIndex = 13;
-            this.btnSet.Text = "Set Angle";
             this.btnSet.UseVisualStyleBackColor = true;
             this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
             // 
             // nudBlaster
             // 
+            resources.ApplyResources(this.nudBlaster, "nudBlaster");
             this.nudBlaster.DecimalPlaces = 1;
-            this.nudBlaster.Location = new System.Drawing.Point(74, 24);
             this.nudBlaster.Maximum = new decimal(new int[] {
-            180,
+            359,
             0,
             0,
             0});
             this.nudBlaster.Minimum = new decimal(new int[] {
-            180,
+            359,
             0,
             0,
             -2147483648});
             this.nudBlaster.Name = "nudBlaster";
-            this.nudBlaster.Size = new System.Drawing.Size(86, 20);
-            this.nudBlaster.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 28);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 13);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Spin";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(166, 26);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 13);
-            this.label2.TabIndex = 15;
-            this.label2.Text = "degrees/frame";
             // 
             // gbMain
             // 
+            resources.ApplyResources(this.gbMain, "gbMain");
+            this.gbMain.Controls.Add(this.pnlRotBox);
             this.gbMain.Controls.Add(this.btnFromGame);
             this.gbMain.Controls.Add(this.nudSpecificSet);
-            this.gbMain.Controls.Add(this.tbAngle);
             this.gbMain.Controls.Add(this.btnSet);
             this.gbMain.Controls.Add(this.pbAngleDisplay);
-            this.gbMain.Location = new System.Drawing.Point(12, 27);
             this.gbMain.Name = "gbMain";
-            this.gbMain.Size = new System.Drawing.Size(479, 128);
-            this.gbMain.TabIndex = 16;
             this.gbMain.TabStop = false;
-            this.gbMain.Text = "Angle Control";
+            // 
+            // pnlRotBox
+            // 
+            resources.ApplyResources(this.pnlRotBox, "pnlRotBox");
+            this.pnlRotBox.BackgroundImage = global::METALTwister.Properties.Resources.rotation_panel_1;
+            this.pnlRotBox.Name = "pnlRotBox";
+            this.pnlRotBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlRotBox_MouseDown);
+            this.pnlRotBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlRotBox_MouseMove);
+            this.pnlRotBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlRotBox_MouseUp);
             // 
             // btnFromGame
             // 
-            this.btnFromGame.Location = new System.Drawing.Point(336, 48);
+            resources.ApplyResources(this.btnFromGame, "btnFromGame");
             this.btnFromGame.Name = "btnFromGame";
-            this.btnFromGame.Size = new System.Drawing.Size(105, 20);
-            this.btnFromGame.TabIndex = 17;
-            this.btnFromGame.Text = "Read from game";
             this.btnFromGame.UseVisualStyleBackColor = true;
             this.btnFromGame.Click += new System.EventHandler(this.btnFromGame_Click);
             // 
             // nudSpecificSet
             // 
-            this.nudSpecificSet.Location = new System.Drawing.Point(159, 48);
+            resources.ApplyResources(this.nudSpecificSet, "nudSpecificSet");
             this.nudSpecificSet.Maximum = new decimal(new int[] {
-            360,
+            359,
             0,
             0,
             0});
             this.nudSpecificSet.Name = "nudSpecificSet";
-            this.nudSpecificSet.Size = new System.Drawing.Size(86, 20);
-            this.nudSpecificSet.TabIndex = 16;
             // 
             // pbAngleDisplay
             // 
-            this.pbAngleDisplay.Location = new System.Drawing.Point(6, 19);
+            resources.ApplyResources(this.pbAngleDisplay, "pbAngleDisplay");
             this.pbAngleDisplay.Name = "pbAngleDisplay";
-            this.pbAngleDisplay.Size = new System.Drawing.Size(100, 100);
-            this.pbAngleDisplay.TabIndex = 12;
             this.pbAngleDisplay.TabStop = false;
             // 
             // gbAuto
             // 
+            resources.ApplyResources(this.gbAuto, "gbAuto");
             this.gbAuto.Controls.Add(this.label1);
             this.gbAuto.Controls.Add(this.nudBlaster);
             this.gbAuto.Controls.Add(this.btnAuto);
             this.gbAuto.Controls.Add(this.label2);
-            this.gbAuto.Location = new System.Drawing.Point(12, 161);
             this.gbAuto.Name = "gbAuto";
-            this.gbAuto.Size = new System.Drawing.Size(305, 64);
-            this.gbAuto.TabIndex = 17;
             this.gbAuto.TabStop = false;
-            this.gbAuto.Text = "Auto Control";
             // 
             // btnAuto
             // 
+            resources.ApplyResources(this.btnAuto, "btnAuto");
             this.btnAuto.BackgroundImage = global::METALTwister.Properties.Resources.mT_Repeat;
-            this.btnAuto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnAuto.Location = new System.Drawing.Point(251, 10);
             this.btnAuto.Name = "btnAuto";
-            this.btnAuto.Size = new System.Drawing.Size(48, 48);
-            this.btnAuto.TabIndex = 10;
             this.btnAuto.UseVisualStyleBackColor = true;
             this.btnAuto.Click += new System.EventHandler(this.ToggleAuto);
             // 
             // gbFreeRoam
             // 
+            resources.ApplyResources(this.gbFreeRoam, "gbFreeRoam");
             this.gbFreeRoam.Controls.Add(this.cbFreeRoam);
-            this.gbFreeRoam.Location = new System.Drawing.Point(323, 161);
             this.gbFreeRoam.Name = "gbFreeRoam";
-            this.gbFreeRoam.Size = new System.Drawing.Size(168, 64);
-            this.gbFreeRoam.TabIndex = 18;
             this.gbFreeRoam.TabStop = false;
-            this.gbFreeRoam.Text = "Free Roam Mode";
             // 
             // cbFreeRoam
             // 
-            this.cbFreeRoam.AutoSize = true;
-            this.cbFreeRoam.Location = new System.Drawing.Point(55, 27);
+            resources.ApplyResources(this.cbFreeRoam, "cbFreeRoam");
             this.cbFreeRoam.Name = "cbFreeRoam";
-            this.cbFreeRoam.Size = new System.Drawing.Size(59, 17);
-            this.cbFreeRoam.TabIndex = 0;
-            this.cbFreeRoam.Text = "Enable";
             this.cbFreeRoam.UseVisualStyleBackColor = true;
             // 
             // menuStrip1
             // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutToolStripMenuItem,
             this.helpToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(503, 24);
-            this.menuStrip1.TabIndex = 19;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // aboutToolStripMenuItem
             // 
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "Help";
             this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
             // 
             // AngleBlaster
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(503, 234);
             this.Controls.Add(this.gbFreeRoam);
             this.Controls.Add(this.gbAuto);
             this.Controls.Add(this.gbMain);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "AngleBlaster";
-            this.Text = "METAL Twister";
-            ((System.ComponentModel.ISupportInitialize)(this.tbAngle)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudBlaster)).EndInit();
             this.gbMain.ResumeLayout(false);
-            this.gbMain.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudSpecificSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbAngleDisplay)).EndInit();
             this.gbAuto.ResumeLayout(false);
@@ -257,7 +209,6 @@
 
         #endregion
         private System.Windows.Forms.Button btnAuto;
-        private System.Windows.Forms.TrackBar tbAngle;
         private System.Windows.Forms.PictureBox pbAngleDisplay;
         private System.Windows.Forms.Button btnSet;
         private System.Windows.Forms.NumericUpDown nudBlaster;
@@ -272,5 +223,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlRotBox;
     }
 }
--- METALTwister/frmAbout.Designer.cs
+++ METALTwister/frmAbout.Designer.cs
@@ -29,93 +29,63 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAbout));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lbVer = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.btnPC = new System.Windows.Forms.Button();
             this.btnWiki = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::METALTwister.Properties.Resources.mT_Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(439, 112);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(79, 127);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(317, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "METAL Twister for M64MM3 Alpha 2";
             // 
             // lbVer
             // 
-            this.lbVer.AutoSize = true;
-            this.lbVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbVer.Location = new System.Drawing.Point(12, 246);
+            resources.ApplyResources(this.lbVer, "lbVer");
             this.lbVer.Name = "lbVer";
-            this.lbVer.Size = new System.Drawing.Size(51, 20);
-            this.lbVer.TabIndex = 2;
-            this.lbVer.Text = "v1.0.1";
-            this.lbVer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(80, 151);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(316, 52);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Spin Mario without having to press extra buttons.\r\nMight have some funny side eff" +
-    "ects. ;)\r\n\r\nNeed help? Check out the Wiki or join the Project Comet Discord.\r\n";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(376, 240);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnPC
             // 
-            this.btnPC.Location = new System.Drawing.Point(195, 240);
+            resources.ApplyResources(this.btnPC, "btnPC");
             this.btnPC.Name = "btnPC";
-            this.btnPC.Size = new System.Drawing.Size(94, 23);
-            this.btnPC.TabIndex = 5;
-            this.btnPC.Text = "Project Comet";
             this.btnPC.UseVisualStyleBackColor = true;
             this.btnPC.Click += new System.EventHandler(this.btnPC_Click);
             // 
             // btnWiki
             // 
-            this.btnWiki.Location = new System.Drawing.Point(295, 240);
+            resources.ApplyResources(this.btnWiki, "btnWiki");
             this.btnWiki.Name = "btnWiki";
-            this.btnWiki.Size = new System.Drawing.Size(75, 23);
-            this.btnWiki.TabIndex = 6;
-            this.btnWiki.Text = "Wiki";
             this.btnWiki.UseVisualStyleBackColor = true;
             this.btnWiki.Click += new System.EventHandler(this.btnWiki_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::METALTwister.Properties.Resources.mT_Logo;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
             // frmAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 275);
             this.Controls.Add(this.btnWiki);
             this.Controls.Add(this.btnPC);
             this.Controls.Add(this.button1);
@@ -124,11 +94,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmAbout";
-            this.Text = "About METAL Twister...";
             this.Load += new System.EventHandler(this.frmAbout_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
